0ab806fc809b5a6eb5b9c3dd27aaa967a0231238
Ruvalter Franca <ruvalter@gmail.com>
2023-10-23 22:34:15 -0300

    feat (OC-37): Update icons

--- app/routes/resources.manifest[.]webmanifest.ts
+++ app/routes/resources.manifest[.]webmanifest.ts
@@ -1,7 +1,7 @@
 import { json } from '@remix-run/node';
 import type { LoaderFunction } from '@remix-run/node';
 
-export let loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async () => {
   return json(
     {
       short_name: 'Orem Club',
@@ -16,7 +16,7 @@ export let loader: LoaderFunction = async () => {
           url: '/',
           icons: [
             {
-              src: '/icons/android-icon-96x96.png',
+              src: '/icons/96x96.png',
               sizes: '96x96',
               type: 'image/png',
               purpose: 'any monochrome',
@@ -26,40 +26,74 @@ export let loader: LoaderFunction = async () => {
       ],
       icons: [
         {
-          src: '/icons/android-icon-36x36.png',
-          sizes: '36x36',
+          src: '/icons/16x16.png',
+          sizes: '16x16',
           type: 'image/png',
-          density: '0.75',
+          density: '1.0',
+          purpose: 'maskable',
         },
         {
-          src: '/icons/android-icon-48x48.png',
+          src: '/icons/32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+          density: '1.0',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/48x48.png',
           sizes: '48x48',
           type: 'image/png',
           density: '1.0',
+          purpose: 'maskable',
         },
         {
-          src: '/icons/android-icon-72x72.png',
+          src: '/icons/58x58.png',
+          sizes: '58x58',
+          type: 'image/png',
+          density: '1.0',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/72x72.png',
           sizes: '72x72',
           type: 'image/png',
           density: '1.5',
+          purpose: 'maskable',
         },
         {
-          src: '/icons/android-icon-96x96.png',
+          src: '/icons/96x96.png',
           sizes: '96x96',
           type: 'image/png',
           density: '2.0',
+          purpose: 'maskable',
         },
         {
-          src: '/icons/android-icon-144x144.png',
+          src: '/icons/144x144.png',
           sizes: '144x144',
           type: 'image/png',
           density: '3.0',
+          purpose: 'maskable',
         },
         {
-          src: '/icons/android-icon-192x192.png',
+          src: '/icons/192x192.png',
           sizes: '192x192',
           type: 'image/png',
           density: '4.0',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/256x256.png',
+          sizes: '256x256',
+          type: 'image/png',
+          density: '1.0',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          density: '5.0',
+          purpose: 'maskable',
         },
       ],
     },
