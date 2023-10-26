79d5da49438ff895a666a33eb4be4fdf37e87a92
Romulo Mourão <romulo.dam@gmail.com>
2023-10-26 08:09:28 -0300

    feat: supabase client config

---
+++ app/utils/server/db.server.ts
@@ -0,0 +1,9 @@
+/* eslint-disable unicorn/prevent-abbreviations */
+import { createClient } from '@supabase/supabase-js';
+
+// see documention about using .env variables
+// https://remix.run/docs/en/v1/guides/envvars#server-environment-variables
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabaseClient = createClient(supabaseUrl!, supabaseKey!);
