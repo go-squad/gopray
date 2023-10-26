1dfa684de1c25487b5f1b238e2ffcbbb7b78175f
Romulo Mourão <romulo.dam@gmail.com>
2023-10-26 08:12:19 -0300

    feat: action to upload avatar

---
+++ app/routes/_protected.profile.upload-avatar.tsx
@@ -0,0 +1,22 @@
+import {
+  unstable_parseMultipartFormData,
+  type ActionArgs,
+  type ActionFunction,
+} from '@remix-run/node';
+import { editUser } from '../services/user.server';
+import { requireUser } from '../services/session.server';
+import { uploadHandler } from '../services/storage/upload.server';
+
+export const action: ActionFunction = async ({ request }: ActionArgs) => {
+  const user = await requireUser(request);
+
+  // get file info back after image upload
+  const formData = await unstable_parseMultipartFormData(
+    request,
+    uploadHandler
+  );
+  const avatarUrl = formData.get('avatar') as string;
+  const userUpdated = await editUser(user.id, { avatarUrl });
+
+  return { userUpdated };
+};
