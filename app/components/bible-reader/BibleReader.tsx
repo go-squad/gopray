a9e2435b622b4c8ce869dced9031d7393744a963
Ruvalter Franca <ruvalter@gmail.com>
2023-10-31 13:09:50 -0300

    feat (OC-25): Add constant

--- app/components/bible-reader/BibleReader.tsx
+++ app/components/bible-reader/BibleReader.tsx
@@ -1,5 +1,6 @@
 import { BookOpenIcon } from '@heroicons/react/24/solid';
 import { useEffect, useState } from 'react';
+import { PASSAGES } from '~/models/constants';
 
 export interface Passage {
   text: string;
@@ -7,39 +8,11 @@ export interface Passage {
 }
 
 const RandomBibleReader = ({ className }: { className?: string }) => {
-  const passages = [
-    {
-      text: 'Não deixemos de reunir-nos como igreja, segundo o costume de alguns, mas procuremos encorajar-nos uns aos outros, ainda mais quando vocês veem que se aproxima o Dia.',
-      verse: 'Hebreus 10:24-25',
-    },
-    {
-      text: 'Não andem ansiosos por coisa alguma, mas em tudo, pela oração e súplicas, e com ação de graças, apresentem seus pedidos a Deus. E a paz de Deus, que excede todo o entendimento, guardará o coração e a mente de vocês em Cristo Jesus.',
-      verse: 'Filipenses 4:6-7',
-    },
-
-    {
-      text: 'Dediquem-se à oração, estejam alerta e sejam agradecidos.',
-      verse: 'Colossenses 4:2',
-    },
-    {
-      text: 'Então vocês clamarão a mim, virão orar a mim, e eu os ouvirei.',
-      verse: 'Jeremias 29:12',
-    },
-    {
-      text: 'Alegrem-se na esperança, sejam pacientes na tribulação, perseverem na oração.',
-      verse: 'Romanos 12:12',
-    },
-    {
-      text: 'Clame a mim e eu responderei e lhe direi coisas grandiosas e insondáveis que você não conhece.',
-      verse: 'Jeremias 33:3',
-    },
-  ];
-
-  const [passage, setPassage] = useState(passages[0]);
+  const [passage, setPassage] = useState(PASSAGES[0]);
 
   useEffect(() => {
-    const randomNumber = Math.floor(Math.random() * passages.length);
-    setPassage(passages[randomNumber]);
+    const randomNumber = Math.floor(Math.random() * PASSAGES.length);
+    setPassage(PASSAGES[randomNumber]);
   }, []);
 
   return (
---
+++ app/models/constants.ts
@@ -0,0 +1,27 @@
+export const PASSAGES = [
+  {
+    text: 'Não deixemos de reunir-nos como igreja, segundo o costume de alguns, mas procuremos encorajar-nos uns aos outros, ainda mais quando vocês veem que se aproxima o Dia.',
+    verse: 'Hebreus 10:24-25',
+  },
+  {
+    text: 'Não andem ansiosos por coisa alguma, mas em tudo, pela oração e súplicas, e com ação de graças, apresentem seus pedidos a Deus. E a paz de Deus, que excede todo o entendimento, guardará o coração e a mente de vocês em Cristo Jesus.',
+    verse: 'Filipenses 4:6-7',
+  },
+
+  {
+    text: 'Dediquem-se à oração, estejam alerta e sejam agradecidos.',
+    verse: 'Colossenses 4:2',
+  },
+  {
+    text: 'Então vocês clamarão a mim, virão orar a mim, e eu os ouvirei.',
+    verse: 'Jeremias 29:12',
+  },
+  {
+    text: 'Alegrem-se na esperança, sejam pacientes na tribulação, perseverem na oração.',
+    verse: 'Romanos 12:12',
+  },
+  {
+    text: 'Clame a mim e eu responderei e lhe direi coisas grandiosas e insondáveis que você não conhece.',
+    verse: 'Jeremias 33:3',
+  },
+];
